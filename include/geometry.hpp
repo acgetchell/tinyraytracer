417c75ad4c03066ebb78bdbade9bd01278d116c0
Adam Getchell <acgetchell@ucdavis.edu>
2019-02-09 19:44:11 -0800

    Headers, tools, and compiler checks

---
+++ include/geometry.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Adam Getchell on 2019-02-09.
+//
+
+#ifndef TINYRAYTRACER_GEOMETRY_HPP
+#define TINYRAYTRACER_GEOMETRY_HPP
+
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <vector>
+
+#endif //TINYRAYTRACER_GEOMETRY_HPP
--- src/tinyraytracer.cpp
+++ src/tinyraytracer.cpp
@@ -2,7 +2,7 @@
 // Created by Adam Getchell on 2019-02-09.
 // Inspired by https://github.com/ssloy/tinyraytracer
 //
-#include <iostream>
+#include <geometry.hpp>
 
 int main()
 {
