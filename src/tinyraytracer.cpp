cb965204fe390e60732323813855d1ff51a57938
Adam Getchell <acgetchell@ucdavis.edu>
2019-02-10 19:24:41 -0800

    Step 5
    Specular lighting with Phong illumination

--- src/tinyraytracer.cpp
+++ src/tinyraytracer.cpp
@@ -15,9 +15,13 @@ struct Light
 
 struct Material
 {
+  Vec2f albedo;
   Vec3f diffuse_color;
-  explicit Material(Vec3f const& color) : diffuse_color(color) {}
-  Material() : diffuse_color() {}
+  float specular_exponent;
+  explicit Material(Vec2f const& a, Vec3f const& color, float const& spec)
+      : albedo(a), diffuse_color(color), specular_exponent(spec)
+  {}
+  Material() : albedo(1, 0), diffuse_color(), specular_exponent() {}
 };
 
 struct Sphere
@@ -43,6 +47,8 @@ struct Sphere
     return t0 >= 0;
   }
 };
+
+Vec3f reflect(Vec3f const& I, Vec3f const& N) { return I - N * 2.f * (I * N); }
 
 bool scene_intersect(Vec3f const& orig, Vec3f const& dir,
                      std::vector<Sphere> const& spheres, Vec3f& hit, Vec3f& N,
@@ -73,13 +79,20 @@ Vec3f cast_ray(Vec3f const& orig, Vec3f const& dir,
   {
     return Vec3f(0.2f, 0.7f, 0.8f);  // background color
   }
-  float diffuse_light_intensity = 0;
+  float diffuse_light_intensity  = 0;
+  float specular_light_intensity = 0;
   for (auto light : lights)
   {
     Vec3f light_dir = (light.position - point).normalize();
     diffuse_light_intensity += light.intensity * std::max(0.f, light_dir * N);
+    specular_light_intensity +=
+        powf(std::max(0.f, -reflect(-light_dir, N) * dir),
+             material.specular_exponent) *
+        light.intensity;
   }
-  return material.diffuse_color * diffuse_light_intensity;  // sphere color
+  return material.diffuse_color * diffuse_light_intensity * material.albedo[0] +
+         Vec3f(1., 1., 1.) * specular_light_intensity *
+             material.albedo[1];  // sphere color, lighting, reflection
 }
 
 void render(std::vector<Sphere> const& spheres,
@@ -99,7 +112,7 @@ void render(std::vector<Sphere> const& spheres,
                tan(fov / 2.f) * width / static_cast<float>(height);
       auto y =
           -(2 * (j + 0.5f) / static_cast<float>(height) - 1) * tan(fov / 2.f);
-      Vec3f dir                  = Vec3f(x, y, -1).normalize();
+      Vec3f dir = Vec3f(x, y, -1).normalize();
       framebuffer[i + j * width] =
           cast_ray(Vec3f(0, 0, 0), dir, spheres, lights);
     }
@@ -110,6 +123,9 @@ void render(std::vector<Sphere> const& spheres,
   ofs << "P6\n" << width << " " << height << "\n255\n";
   for (size_t i = 0; i < height * width; ++i)
   {
+    Vec3f& c   = framebuffer[i];
+    float  max = std::max(c[0], std::max(c[1], c[2]));
+    if (max > 1) c = c * (1.f / max);
     for (size_t j = 0; j < 3; j++)
     {
       ofs << static_cast<char>(255 *
@@ -121,8 +137,8 @@ void render(std::vector<Sphere> const& spheres,
 
 int main()
 {
-  Material ivory(Vec3f(0.4f, 0.4f, 0.3f));
-  Material red_rubber(Vec3f(0.3f, 0.1f, 0.1f));
+  Material ivory(Vec2f(0.6f, 0.3f), Vec3f(0.4f, 0.4f, 0.3f), 50.);
+  Material red_rubber(Vec2f(0.9f, 0.1f), Vec3f(0.3f, 0.1f, 0.1f), 10.);
 
   std::vector<Sphere> spheres;
   spheres.emplace_back(Sphere(Vec3f(-3, 0, -16), 2, ivory));
@@ -132,6 +148,8 @@ int main()
 
   std::vector<Light> lights;
   lights.emplace_back(Light(Vec3f(-20, 20, 20), 1.5));
+  lights.emplace_back(Light(Vec3f(30, 50, -25), 1.8f));
+  lights.emplace_back(Light(Vec3f(30, 20, 30), 1.7f));
 
   render(spheres, lights);
 
